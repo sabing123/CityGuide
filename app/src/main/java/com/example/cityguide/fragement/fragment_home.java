9ade8b66bc9f99eda4baaf247681b44b7ab036a9
Sabin <sabingautam05@gmail.com>
2020-12-27 21:01:58 +0545

    complete Map

--- app/src/main/java/com/example/cityguide/fragement/fragment_home.java
+++ app/src/main/java/com/example/cityguide/fragement/fragment_home.java
@@ -1,22 +1,54 @@
 package com.example.cityguide.fragement;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.cityguide.R;
 
-public class fragment_home extends Fragment {
+public class fragment_home extends Fragment implements LocationListener {
+
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected Context context;
+    TextView txtLat;
+    String lat;
+    String provider;
+    protected String latitude, longitude;
+    protected boolean gps_enabled, network_enabled;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        txtLat = (TextView) view.findViewById(R.id.textview1);
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (android.location.LocationListener) this);
+        return view;
     }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+    }
+
 }
--- app/src/main/java/com/example/cityguide/fragement/fragment_places.java
+++ app/src/main/java/com/example/cityguide/fragement/fragment_places.java
@@ -1,34 +1,68 @@
 package com.example.cityguide.fragement;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Adapter;
-
+import com.example.cityguide.MapsActivity;
 import com.example.cityguide.R;
+import com.example.cityguide.StrictMode.strictmodeclass;
 import com.example.cityguide.fragement.PlaceTabViewFragement.TrekkingPlaceFragment;
 import com.example.cityguide.fragement.PlaceTabViewFragement.famousPlaceFragment;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 
 public class fragment_places extends Fragment {
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+
+    Geocoder geocoder;
+    List<Address> addresses;
+    
+    Double latitude = 27.6999007;
+    Double longitude = 85.2734998;
+
+    TextView txtLat, txtadress;
+    Button btnsearchplaces;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_places, container, false);
+        View view = inflater.inflate(R.layout.fragment_places, container, false);
 
         // Setting ViewPager for each Tabs
         ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewpager);
@@ -37,8 +71,36 @@ public class fragment_places extends Fragment {
         TabLayout tabs = (TabLayout) view.findViewById(R.id.result_tabs);
         tabs.setupWithViewPager(viewPager);
 
+        txtLat = (TextView) view.findViewById(R.id.currentLocation);
+        btnsearchplaces =  view.findViewById(R.id.btnsearchplaces);
+
+        txtadress = view.findViewById(R.id.currentaddress);
+
+        btnsearchplaces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), MapsActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+
+        geocoder = new Geocoder(getContext(), Locale.getDefault());
+        try {
+
+            addresses = geocoder.getFromLocation(latitude,longitude,1);
+            String address = addresses.get(0).getLocality();
+            String caddress = addresses.get(0).getFeatureName();
+            txtadress.setText(caddress + address + addresses.get(0).getCountryName());
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return view;
     }
+
+
 
     private void setupViewPager(ViewPager viewPager) {
         Adapter adapter = new Adapter(getChildFragmentManager());
